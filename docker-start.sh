7a92f21d6a46fbd873c2df690570f0950665b8b1
Lorenzo Facchin <903942@stud.unive.it>
2024-12-15 16:59:28 +0100

    mod docker start

--- docker-start.sh
+++ docker-start.sh
@@ -1,4 +1,6 @@
 docker exec --workdir /var/www/html management_service composer install --no-interaction --optimize-autoloader
+docker exec --workdir /var/www/html management_service php bin/console secrets:decrypt-to-local --force --env=dev
+docker exec --workdir /var/www/html management_service composer dump-env dev
 docker exec --workdir /var/www/html management_service php bin/console doctrine:migrations:migrate --no-interaction
 docker exec --workdir /var/www/html management_service php bin/console cache:clear --quiet
 docker exec --workdir /var/www/html management_service php bin/console c:w --quiet
