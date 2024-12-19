8a315fdf6c37f057a1c4113027e121510ab1a765
Lorenzo903942 <903942@stud.unive.it>
2024-12-19 22:14:18 +0100

    note in sh

--- docker-start.sh
+++ docker-start.sh
@@ -2,6 +2,9 @@ docker exec --workdir /var/www/html service-management composer install --no-int
 docker exec --workdir /var/www/html service-management php bin/console secrets:decrypt-to-local --force --env=dev
 docker exec --workdir /var/www/html service-management composer dump-env dev
 docker exec --workdir /var/www/html service-management php bin/console lexik:jwt:generate-keypair
+
+# THE FOLLOWING LINE IT'S NOT NECESSARY IF YOU'VE EXECUTED SCRIPT IN DATABASE REPOSITORY
 docker exec --workdir /var/www/html service-management php bin/console doctrine:migrations:migrate --no-interaction
+
 docker exec --workdir /var/www/html service-management php bin/console cache:clear --quiet
 docker exec --workdir /var/www/html service-management php bin/console c:w --quiet
