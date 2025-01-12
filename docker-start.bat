cc697f4eb45dcb813568933b0d260f39d57f443a
Kazin Fedor <908385@stud.unive.it>
2025-01-12 19:57:48 +0100

    docker-start.bat
    Added .bat file that duplicated docker-start.sh for the Windows users

---
+++ docker-start.bat
@@ -0,0 +1,6 @@
+docker exec --workdir /var/www/html service-management composer install --no-interaction --optimize-autoloader
+docker exec --workdir /var/www/html service-management php bin/console secrets:decrypt-to-local --force --env=dev
+docker exec --workdir /var/www/html service-management composer dump-env dev
+docker exec --workdir /var/www/html service-management php bin/console lexik:jwt:generate-keypair
+docker exec --workdir /var/www/html service-management php bin/console cache:clear --quiet
+docker exec --workdir /var/www/html service-management php bin/console c:w --quiet
